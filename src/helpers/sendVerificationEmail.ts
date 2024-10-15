cc4053195e0d02c341024fe47246ab482322cdcb
AhmedAfzal2089 <ameafzal@gmail.com>
2024-10-15 22:15:00 +0500

    add email verificaion

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
