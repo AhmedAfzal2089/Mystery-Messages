8cd7e8e667a978bb27efb1a8c3d47eef79ab61a2
AhmedAfzal2089 <ameafzal@gmail.com>
2024-10-19 21:59:07 +0500

    initialize toast

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import *as z  from "zod" // it will import all the exports in this app
+import Link from "next/link"
+import { useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/hooks/use-toast"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+
+const SignIn = () => {
+  const{toast}= useToast()
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const debouncedUsername = useDebounceValue(username,300) //to check the username is available or not to reduce the server load
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      
+    </div>
   )
 }
+
+export default SignIn
